376cf489ed316ac12c18dff8e76f36002d2c3785
John Gallagher <john@synapticmishap.co.uk>
2017-01-29 21:12:48 +0000

    Add hooks.

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pyenv virtualenv {{cookiecutter.project_slug}}
+pyenv local {{cookiecutter.project_slug}}
+pip install -r requirements.txt
+
+git init .
+git add -A
+git commit -m "Initial commit."
+hub create -p {{cookiecutter.github_repo}}
+git push origin master
