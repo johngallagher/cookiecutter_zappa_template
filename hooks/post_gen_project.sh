ead4977b8a51428ac959c6c7bdc130eb952945b9
John Gallagher <john@synapticmishap.co.uk>
2017-02-18 15:46:36 +0000

    Ignore workspace once we've imported it.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,6 +5,9 @@ pyenv virtualenv {{cookiecutter.project_slug}}
 pyenv local {{cookiecutter.project_slug}}
 pip install -r requirements.txt
 
+# There's a starter workspace in the template but we don't want to track it from hereon out
+echo .idea/workspace.xml >> .gitignore
+
 git init .
 git add -A
 git commit -m "Initial commit."
