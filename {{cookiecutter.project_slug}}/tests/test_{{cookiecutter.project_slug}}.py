af1538d66c05a9063635dc14b1075066e947bb99
John Gallagher <john@synapticmishap.co.uk>
2017-02-18 17:18:06 +0000

    More boilerplate.

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,6 +1,13 @@
 import unittest
 
 class Test{{cookiecutter.project_camelcase}}(unittest.TestCase):
+    maxDiff = None
+    
+    # @unittest.skipIf(os.environ.get('FAST'), "Only running fast tests.") # Uncomment for a slow test
     def test_something(self):
-        self.assertEqual('something', 'something else')
+        input = {
+            'key': 'value'
+        }
+        output = {{cookiecutter.project_slug}}.run(input, { })
+        self.assertEqual(output, { 'key': 'value' })
 
