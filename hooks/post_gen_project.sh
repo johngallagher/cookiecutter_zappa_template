fab43dc80c14258ca5485a0198bfeab51e8fa987
John Gallagher <john@synapticmishap.co.uk>
2017-02-18 19:48:27 +0000

    Create setup script.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-pyenv local 2.7.12
-pyenv virtualenv {{cookiecutter.project_slug}}
-pyenv local {{cookiecutter.project_slug}}
-pip install -r requirements.txt
+bin/setup
 
-# There's a starter workspace in the template but we don't want to track it from hereon out
+# There's a starter workspace in the template but we don't want to track from now on
 echo .idea/workspace.xml >> .gitignore
 
 git init .
